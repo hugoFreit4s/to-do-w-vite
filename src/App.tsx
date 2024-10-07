5acdba5e7cadca7095a97424c96b44aecb49910e
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-10-07 10:47:09 -0300

    Task del feature working

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,41 @@ import Task from './Task';
 
 function App() {
   const [name, setName] = useState('');
-  const [database, setDatabase] = useState<string[]>([]);
+  const [database, setDatabase] = useState<{ tName: string, id: string }[]>([]);
 
+  function removeTask(tId: string) {
+    const tempArr: { tName: string, id: string }[] = [];
+    database.forEach(x => {
+      if (x.id !== tId) tempArr.push(x);
+    });
+    setDatabase([...tempArr]);
+  }
   return (
     <>
-      <input type="text" onChange={e => setName(e.target.value)} />
+      <input type="text" onChange={e => {
+        if (e.target.value.length >= 4) {
+          setName(e.target.value);
+        } else {
+          setName('No title');
+        }
+      }} />
       <button onClick={() => {
-        setDatabase(prevDatabase => [...prevDatabase, name]);
+        if (name === '') {
+          setDatabase(prev => [...prev, { tName: 'No title', id: crypto.randomUUID() }]);
+        } else {
+          setDatabase(prev => [...prev, { tName: name, id: crypto.randomUUID() }]);
+        }
         console.log(database);
       }}>add task</button>
 
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
         {database.map((task) => (
-          <Task taskName={task} />
+          <div>
+            <Task taskName={task.tName} />
+            <button onClick={() => removeTask(task.id)}>Del task</button>
+          </div>
         ))}
-      </div>
+      </div >
     </>
   );
 }
--- src/Task.tsx
+++ src/Task.tsx
@@ -2,7 +2,9 @@ type Props = {
     taskName: string;
 }
 const Task = ({ taskName }: Props) => {
-    return <p style={{ backgroundColor: 'gray', width: 'fit-content' }}>{taskName}</p>
+    return (
+        <p style={{ backgroundColor: 'gray', width: 'fit-content' }}>{taskName}</p>
+    )
 }
 
 export default Task;
